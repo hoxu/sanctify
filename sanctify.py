6d44de225cb341bd6244f5a11d20680eeb9ec037
Heikki Hokkanen <hoxu@users.sf.net>
2016-03-05 12:58:01 +0200

    Use nargs=argparse.REMAINDER for subcommand arguments
    Leaves '--' intact, needed for wrappers.

--- sanctify.py
+++ sanctify.py
@@ -40,13 +40,13 @@ subparsers = parser.add_subparsers(help='sub-command help')
 # run parser
 parser_run = subparsers.add_parser('run', help='Run a job')
 parser_run.add_argument('job', help='Path to job script')
-parser_run.add_argument('arguments', nargs='*', help='job arguments')
+parser_run.add_argument('arguments', nargs=argparse.REMAINDER, help='job arguments')
 parser_run.set_defaults(func=run)
 
 # wrapper parser
 parser_wrapper = subparsers.add_parser('wrapper', help='Run a wrapper')
 parser_wrapper.add_argument('name', help='Wrapper name')
-parser_wrapper.add_argument('arguments', nargs='*', help='wrapper arguments')
+parser_wrapper.add_argument('arguments', nargs=argparse.REMAINDER, help='wrapper arguments')
 parser_wrapper.set_defaults(func=wrapper)
 
 if __name__ == '__main__':
